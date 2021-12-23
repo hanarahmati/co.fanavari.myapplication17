75c775e8a4022a5e75146744f9a51c6fc78936c4
Hana <h.rahmatzehi@behsacorp.com>
2021-12-23 17:58:31 +0330

    session 7

--- app/src/main/java/co/fanavari/myapplication/DashboardActivity.kt
+++ app/src/main/java/co/fanavari/myapplication/DashboardActivity.kt
@@ -1,16 +1,24 @@
 package co.fanavari.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatImageButton
+import co.fanavari.myapplication.databinding.ActivityDashboardBinding
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.card.MaterialCardView
 
 class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashboardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        /*setContentView(R.layout.activity_dashboard)
 
         val backB: AppCompatImageButton = findViewById(R.id.backB)
         val logoutB: AppCompatImageButton = findViewById(R.id.logoutB)
@@ -25,9 +33,32 @@ class DashboardActivity : AppCompatActivity() {
 
         backB.setOnClickListener {
             Toast.makeText(this,getString(R.string.class_name),Toast.LENGTH_LONG).show()
+        }*/
+
+        /*binding.layoutCards.setOnClickListener {
+            showToast("this is layoutCard")
+
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }*/
+
+        binding.layoutCards.setOnClickListener {
+            val mentorName: String = binding.textViewMentorName.text.toString()
+            val intent = Intent(this,IntentExampleActivity::class.java)
+
+            intent.putExtra("classNumber",17)
+            intent.putExtra(Constants.MENTOR_NAME,mentorName)
+            startActivity(intent)
         }
 
+        binding.lifeCycleCard.setOnClickListener {
+            var intent = Intent(this,LifeCycleActivity::class.java)
+            startActivity(intent)
+        }
 
+    }
+
+    fun openLifeCycle(view: View){
 
     }
 }
