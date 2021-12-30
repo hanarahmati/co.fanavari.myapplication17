1d98ba7debc4e6c111e9058865cbc1b73574fc8c
Hana <h.rahmatzehi@behsacorp.com>
2021-12-30 19:10:19 +0330

    session9 initial architecture

---
+++ app/src/main/java/co/fanavari/myapplication/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,12 @@
+package co.fanavari.myapplication.ui.gallery
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import co.fanavari.myapplication.R
+
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+   }
