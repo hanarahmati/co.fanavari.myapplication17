154b5dcf2e922c1a6f68633af70ebcc566bdfb41
Hana <h.rahmatzehi@behsacorp.com>
2022-02-05 00:39:46 +0330

    session 11 part 5

--- app/src/main/java/co/fanavari/myapplication/ui/task/TasksFragment.kt
+++ app/src/main/java/co/fanavari/myapplication/ui/task/TasksFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import co.fanavari.myapplication.R
+import co.fanavari.myapplication.databinding.FragmentTasksBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -14,4 +16,24 @@ import dagger.hilt.android.AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
     private val viewModel: TasksViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTasksBinding.bind(view)
+
+        val taskAdapter = TasksAdapter()
+
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = taskAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+
+        viewModel.tasks.observe(viewLifecycleOwner) {
+            taskAdapter.submitList(it)
+        }
+    }
 }
--- app/src/main/java/co/fanavari/myapplication/ui/task/TasksViewModel.kt
+++ app/src/main/java/co/fanavari/myapplication/ui/task/TasksViewModel.kt
@@ -1,11 +1,16 @@
 package co.fanavari.myapplication.ui.task
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import co.fanavari.myapplication.data.TaskDao
+import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.scopes.ViewModelScoped
+import javax.inject.Inject
 
-
-class TasksViewModel @ViewModelScoped constructor(
+@HiltViewModel
+class TasksViewModel @Inject constructor(
     private val taskDao: TaskDao
 ): ViewModel(){
+
+    val tasks = taskDao.getTask().asLiveData()
 }
