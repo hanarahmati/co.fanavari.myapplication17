651ad346f230668ee9aa6cc1318c6c1875160ed3
Hana <h.rahmatzehi@behsacorp.com>
2022-02-09 18:57:47 +0330

    session 11 part 7

--- app/src/main/java/co/fanavari/myapplication/data/task/TaskDao.kt
+++ app/src/main/java/co/fanavari/myapplication/data/task/TaskDao.kt
@@ -1,6 +1,7 @@
 package co.fanavari.myapplication.data.task
 
 import androidx.room.*
+import co.fanavari.myapplication.ui.task.SortOrder
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -9,8 +10,20 @@ interface TaskDao {
     /*@Query("SELECT * FROM task_table")
     fun getTask(): Flow<List<Task>>*/
 
-    @Query("SELECT * FROM task_table WHERE name LIKE '%' || :searchQuery || '%' ORDER BY important DESC")
-    fun getTasks(searchQuery: String): Flow<List<Task>>
+    /*@Query("SELECT * FROM task_table WHERE name LIKE '%' || :searchQuery || '%' ORDER BY important DESC")
+    fun getTasks(searchQuery: String): Flow<List<Task>>*/
+
+    fun getTasks(query: String, sortOrder: SortOrder, hideCompleted: Boolean): Flow<List<Task>> =
+        when(sortOrder) {
+            SortOrder.BY_DATE -> getTasksSortedByDateCreated(query, hideCompleted)
+            SortOrder.BY_NAME -> getTasksSortedByName(query, hideCompleted)
+        }
+
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :query || '%' ORDER BY important DESC, name")
+    fun getTasksSortedByName(query: String, hideCompleted: Boolean): Flow<List<Task>>
+
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :query || '%' ORDER BY important DESC, created")
+    fun getTasksSortedByDateCreated(query: String, hideCompleted: Boolean): Flow<List<Task>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(task: Task)
--- app/src/main/java/co/fanavari/myapplication/ui/task/TasksViewModel.kt
+++ app/src/main/java/co/fanavari/myapplication/ui/task/TasksViewModel.kt
@@ -2,9 +2,11 @@ package co.fanavari.myapplication.ui.task
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.room.Query
 import co.fanavari.myapplication.data.task.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
@@ -16,10 +18,23 @@ class TasksViewModel @Inject constructor(
     //val tasks = taskDao.getTask().asLiveData()
 
     val searchQuery = MutableStateFlow("")
+    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
+    val hideCompleted =  MutableStateFlow(false)
 
-    private val tasksFlow = searchQuery.flatMapLatest {
+    /*private val tasksFlow = searchQuery.flatMapLatest {
         taskDao.getTasks(it)
+    }*/
+
+    private val tasksFlow = combine(
+        searchQuery, sortOrder, hideCompleted
+    ){
+        query,  sortOrder, hideCompleted ->
+        Triple(query, sortOrder, hideCompleted)
+    }.flatMapLatest { (query, sortOrder, hideCompleted) ->
+        taskDao.getTasks(query, sortOrder, hideCompleted)
     }
 
     val tasks = tasksFlow.asLiveData()
 }
+
+enum class SortOrder { BY_NAME, BY_DATE}
